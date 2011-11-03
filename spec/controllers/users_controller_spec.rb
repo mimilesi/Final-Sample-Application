49682cba36f9e58296f7f387c76548daad611f18
Chris Radcliff <chris.radcliff@gmail.com>
2011-11-03 14:52:43 -0700

    Finished basic sign-up form.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -49,30 +49,57 @@ describe UsersController do
   end
   
   describe "POST 'create'" do
+
+    describe "failure" do
     
-    before(:each) do
-      @attr = {
-        :name => "",
-        :email => "",
-        :password => "",
-        :password_confirmation => ""
-      }
-    end
-    
-    it "should not create a user" do
-      lambda do
+      before(:each) do
+        @attr = {
+          :name => "",
+          :email => "",
+          :password => "",
+          :password_confirmation => ""
+        }
+      end
+      
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+      
+      it "should have the right title" do
         post :create, :user => @attr
-      end.should_not change(User, :count)
+        response.should have_selector("title", :content => "Sign Up")
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
     end
     
-    it "should have the right title" do
-      post :create, :user => @attr
-      response.should have_selector("title", :content => "Sign Up")
-    end
-    
-    it "should render the 'new' page" do
-      post :create, :user => @attr
-      response.should render_template('new')
+    describe "success" do
+      
+      before(:each) do
+        @attr = {
+          :name => "New User",
+          :email => "user@example.com",
+          :password => "sekretpassword",
+          :password_confirmation => "sekretpassword"
+        }
+      end
+      
+      it "should create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
     end
     
   end
