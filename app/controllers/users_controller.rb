e407987d8f295ce56ec0cad73852bdba010aca8c
Chris Radcliff <chris.radcliff@gmail.com>
2011-11-03 14:24:40 -0700

    Added a basic signup page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def new
     @title = "Sign Up"
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
     @title = @user.name
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # TODO handle a successful save
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
+  
 end
