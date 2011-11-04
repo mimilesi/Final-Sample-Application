b48fc93dad6a362f59614be3e45865df8f270f43
Chris Radcliff <chris.radcliff@gmail.com>
2011-11-04 14:49:16 -0700

    Finished basic user updates.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,4 +27,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = "Edit User"
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Your profile has been updated."
+      redirect_to @user
+    else
+      @title = "Edit User"
+      @user.password = nil
+      @user.password_confirmation = nil
+      render 'edit'
+    end
+  end
+  
 end
