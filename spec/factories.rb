bca8ccf78754b0f0dd0dca32e1963d7bdadb283c
Marianna Milesi <mm00128@surrey.ac.uk>
2012-01-11 02:08:02 +0000

    Setup Factories.rb Example User

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,9 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name                  "Chris Radcliff"
-  user.email                 "dude@globalspin.com"
-  user.password              "foobar"
-  user.password_confirmation "foobar"
+  user.name                  "Marianna Milesi"
+  user.email                 "mm00128@surrey.ac.uk"
+  user.password              "surrey"
+  user.password_confirmation "surrey"
 end
 
 Factory.sequence :email do |n|
